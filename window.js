993abed5ec906d48c94efc10a0b0c2e15cc22e2a
Sima <sima.amini@outlook.com>
2018-10-03 13:53:16 +0330

    Adding edit ,delete and done functions and elements

--- window.js
+++ window.js
@@ -1,32 +1,73 @@
-// Add new task
 let taskInput = document.getElementById('task-input');
-taskInput.focus()
+taskInput.focus();
+let editMode = false;
+
+// Add new task
+add = () => {
+  let newTask = taskInput.value;
+  //check if the input is empty
+  if (newTask == "") {
+    alert('Enter a task');
+
+  }
+  if (!editMode) {
+    //make the input empty after submit
+    taskInput.value = '';
+
+    let pTag = document.createElement('div');
+    let taskListBlock = document.getElementById('taskListBlock');
+
+    pTag.innerHTML = `
+      <div id = "${new Date().getTime()}">
+      - <span>${newTask}</span>
+      <div class="actions">
+      <a href="#" class="deleteBtn" onclick="deleteTask(this.parentNode.parentNode.id)"> 
+      <i class="material-icons">
+      clear
+      </i>
+       </a>
+      <a href="#" class="editBtn" onclick="editTask(this.parentNode.parentNode.id)">
+      <i class="material-icons">
+      edit
+      </i>
+      </a>
+      <a href="#"  class="doneBtn" onclick="doneTask(this.parentNode.parentNode.id)">
+      <i class="material-icons">
+      done
+      </i>
+      </a>
+      </div>
+      </div>`;
+
+    taskListBlock.appendChild(pTag);
+
+  } else {
+    document.getElementById(this.editID).children[0].innerHTML = taskInput.value;
+    taskInput.value = '';
+    editMode = false;
+
+  }
+}
+
+deleteTask = (id) => {
+  document.getElementById(id).parentNode.remove();
+}
+
+editTask = (id) => {
+  taskInput.focus();
+  taskInput.value = document.getElementById(id).children[0].innerHTML;
+  editMode = true;
+  return editID = id;
+}
+doneTask = (id) => {
+  console.log(id);
+  document.getElementById(id).children[0].style.textDecoration = "line-through";
+  document.getElementById(id).children[0].style.textDecorationColor = "green";
+}
 
 taskInput.addEventListener('keyup', (event) => {
   event.preventDefault();
   if (event.keyCode === 13) {
-    let newTask = document.getElementById('task-input').value;
-
-    //check if the input is empty
-    if (newTask == "") {
-      alert('Enter a task');
-
-    } else {
-      //make the input empty after submit
-      document.getElementById('task-input').value = '';
-
-      // keep focus on the input
-
-      console.log(newTask);
-
-      let pTag = document.createElement('p');
-      let taskTag = document.createElement('span');
-      let task = document.createTextNode(newTask);
-      let taskListBlock = document.getElementById('taskListBlock');
-
-      taskTag.appendChild(task);
-      pTag.appendChild(taskTag);
-      taskListBlock.appendChild(pTag);
-    }
+    add();
   }
 })
